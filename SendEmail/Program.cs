77bd0e2d99cca5a1506eb82992be517f1d465437
Jeff <morbius1st@users.noreply.github.com>
2022-05-29 11:09:32 -0700

    update

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -23,6 +23,54 @@ namespace SendEmail
 			Console.ReadKey();
 		}
 
+
+		private static void SendMailByMyProcess()
+		{
+			// attachment does not work
+			//			string mailto =
+			//				@"mailto:jeff.stuyvesant@cyberstudio.pro?subject=subject of message&Body=This is a body of a message"
+			//				+ "&Attachment=D:\\Users\\Jeff\\Documents\\Programming\\VisualStudioProjects\\SendEmail\\SendEmail\\TextFile.txt";
+
+			// this will not work as, i guess, the path is too long
+			string body; // =
+			//				"User Name\t|\tjeffs@architectsorange.com\r\nUser Id\t|\tTVFGJX5H67P7\r\nAll Users Addins Location\t|\tC:\\ProgramData\\Autodesk\\Revit\\Addins\\2018\r\nCurrent User Addins Location\t|\tC:\\Users\\jeffs\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2018\r\nShared Parameters Filename\t|\tD:\\Users\\Jeff\\OneDrive\\Office\\Drawings & Revit\\Revit Projects\\Revit Configuration\\Share Parameters-1.txt\r\nSub Version Number\t|\t2018.0\r\nVersion Build\t|\t20170223_1515(x64)\r\nRecording Journal Filename\t|\tC:\\Users\\jeffs\\AppData\\Local\\Autodesk\\Revit\\Autodesk Revit 2018\\Journals\\journal.0346.txt\r\nVersion Name\t|\tAutodesk Revit 2018\r\nVersion Number\t|\t2018\r\nActive AddIn Name\t|\tApplication HelpDesk\r\nLanguage\t|\tEnglish_USA\r\nProduct\t|\tRevit\r\nDocument Path Name\t|\tD:\\Users\\Jeff\\OneDrive\\Office\\Drawings & Revit\\Revit Projects\\Revit Configuration\\ConfigTest.rvt\r\nDocument Title\t|\tConfigTest.rvt\r\nActive View\t|\tSheet: CS.0-0 - COVER SHEET(COVER SHEET)\r\nDocument Set\t|\tName\r\nDocument Set Item 0\t|\tAutodesk.Revit.DB.Document (Autodesk.Revit.DB.Document)\r\nLibrary Paths\t|\tPath\r\nLibrary Path 0\t|\tC:\\ProgramData\\Autodesk\\RVT 2018\\Libraries\\US Imperial\\Detail Items\r\nLibrary Path 1\t|\tC:\\ProgramData\\Autodesk\\RVT 2018\\Libraries\\US Imperial\\\r\nServer Network Hosts\t|\tHost Name\r\nLoaded Applications 0\t|\tAutodesk.RaaS.RevitAddin.EApplication (Autodesk.RaaS.RevitAddin.EApplication)\r\nLoaded Applications 1\t|\tAutoLoader.AutoLoader (AutoLoader.AutoLoader)\r\nLoaded Applications 2\t|\tConnectedDesktopUI.ServerApp (ConnectedDesktopUI.ServerApp)\r\nLoaded Applications 3\t|\tAutodesk.CoordinationModelUI.CoordinationModelUIApplication (Autodesk.CoordinationModelUI.CoordinationModelUIApplication)\r\nLoaded Applications 4\t|\tEnergyAnalysis.Application (EnergyAnalysis.Application)\r\nLoaded Applications 5\t|\tFabricationPartBrowser.FabPartBrowserApplication (FabricationPartBrowser.FabPartBrowserApplication)\r\nLoaded Applications 6\t|\tFittingAndAccessoryCalculationUIServers.ServerApp (FittingAndAccessoryCalculationUIServers.ServerApp)\r\nLoaded Applications 7\t|\tBIM.IFC.Export.UI.IFCCommandOverrideApplication (BIM.IFC.Export.UI.IFCCommandOverrideApplication)\r\nLoaded Applications 8\t|\tKeynoteUIServer.ServerApp (KeynoteUIServer.ServerApp)\r\nLoaded Applications 9\t|\tMemberForcesUI.ServerApp (MemberForcesUI.ServerApp)\r\nLoaded Applications 10\t|\tBIM.Numbering.UI.NumberingUIApplication (BIM.Numbering.UI.NumberingUIApplication)\r\nLoaded Applications 11\t|\tPointCloudSnappingServer.Application (PointCloudSnappingServer.Application)\r\nLoaded Applications 12\t|\tPressureLossReport.PressureLossReportApplication (PressureLossReport.PressureLossReportApplication)\r\nLoaded Applications 13\t|\tRebarMorphedServer.RebarUpdateApp (RebarMorphedServer.RebarUpdateApp)\r\nLoaded Applications 14\t|\tSectionProperties.ServerApp (SectionProperties.ServerApp)\r\nLoaded Applications 15\t|\tSiteLinker.SiteExportApp (SiteLinker.SiteExportApp)\r\nLoaded Applications 16\t|\tSpaceNaming.Command (SpaceNaming.Command)\r\nLoaded Applications 17\t|\tRvtParameterOrganizer.ExtApp (RvtParameterOrganizer.ExtApp)\r\nLoaded Applications 18\t|\tDluxMeasure.AppRibbon (DluxMeasure.AppRibbon)\r\nLoaded Applications 19\t|\tRevit.PrintHarness.PrintHarnessApplication (Revit.PrintHarness.PrintHarnessApplication)\r\nLoaded Applications 20\t|\tAutodesk.Revit.UI.Collaborate.CollaborateApplication (Autodesk.Revit.UI.Collaborate.CollaborateApplication)\r\nLoaded Applications 21\t|\tAutodesk.Revit.UI.CollaborateBrowser.CollaborateBrowserApplication (Autodesk.Revit.UI.CollaborateBrowser.CollaborateBrowserApplication)\r\nLoaded Applications 22\t|\tSkyscraperClientHost.SkyscraperClientHostApplication (SkyscraperClientHost.SkyscraperClientHostApplication)\r\nLoaded Applications 23\t|\teTransmitForRevit.eTransmitApplication (eTransmitForRevit.eTransmitApplication)\r\nLoaded Applications 24\t|\tBIMStandardsManager.ExternalApp (BIMStandardsManager.ExternalApp)\r\nLoaded Applications 25\t|\tWorkSharingLib.ExternalApplication (WorkSharingLib.ExternalApplication)\r\nLoaded Applications 26\t|\tWorksharingCommand.WorksharingApp (WorksharingCommand.WorksharingApp)\r\nLoaded Applications 27\t|\tAutodesk.Revit.SmartMonkeyPlugin.SmartMonkeyApplication (Autodesk.Revit.SmartMonkeyPlugin.SmartMonkeyApplication)\r\nLoaded Applications 28\t|\tSharedCreate.ApplicationRibbon (SharedCreate.ApplicationRibbon)\r\nLoaded Applications 29\t|\tCTC.RET.SuiteExternalApp.App (CTC.RET.SuiteExternalApp.App)\r\nLoaded Applications 30\t|\tCTC.RET.SuiteExternalApp.App (CTC.RET.SuiteExternalApp.App)\r\nLoaded Applications 31\t|\tSharedCode.ApplicationRibbon (SharedCode.ApplicationRibbon)\r\nLoaded Applications 32\t|\tDynamo.Applications.VersionLoader (Dynamo.Applications.VersionLoader)\r\nLoaded Applications 33\t|\tAutodesk.Revit.Addin.ExportViewSelector.ExportViewSelectorApp (Autodesk.Revit.Addin.ExportViewSelector.ExportViewSelectorApp)\r\nLoaded Applications 34\t|\tFormItConverter.Ribbon (FormItConverter.Ribbon)\r\nLoaded Applications 35\t|\tHelpDesk.RibbonApp (HelpDesk.RibbonApp)\r\n";
+
+
+			// works!!!
+			body =
+				"DIAGNOSTIC INFO\r\n===============\r\nUSER: Cecilia & Cardenas\r\nEMAIL: ccardenas@aoarchitects.com\r\nCOMPUTER: OK-3025\r\nREVIT VERSION: Autodesk Revit 2018 | 20190510_1515(x64)\r\nTOTAL MEMORY: 32 GB\r\nFREE MEMORY: 23.6 GB\r\n\r\nLOCAL MODEL: C:\\KTGY-Revit2018Projects\\170394_Alameda Point_Block 6_ccardenas.rvt\r\nCENTRAL MODEL: \\\\corp.ktgy.com\\global\\Oakland\\Projects\\2017\\170394 Alameda Point TH Block 6\\Construction Drawings\\Revit\\170394_Alameda Point_Block 6.rvt\r\nMODEL FILE SIZE: 322.8 MB\r\nREVIT SERVER ACCELERATOR: OAKREVIT1\r\n";
+
+			string data = Uri.EscapeDataString(body);
+			body = Uri.EscapeUriString(body).Replace("&", "%26");
+
+
+			// this works when the body is shorter
+			string mailto =
+				@"mailto:jeff.stuyvesant@cyberstudio.pro?subject=subject of message&Body=";
+
+			mailto = Uri.EscapeUriString(mailto) + body;
+
+			Process myprocess = new Process();
+
+			myprocess.StartInfo.FileName = mailto;
+			myprocess.StartInfo.UseShellExecute = true;
+			myprocess.StartInfo.RedirectStandardOutput = false;
+
+			Console.WriteLine("as data:");
+			Console.WriteLine(data);
+
+
+			Console.WriteLine("\n\nas uri:");
+			Console.WriteLine(mailto);
+
+			myprocess.Start();
+
+		}
+
+
+
+		// other tests
+
 		private static void TestInfo()
 		{
 			try
@@ -53,55 +101,6 @@ namespace SendEmail
 			var port = smtp.Port;
 			Console.WriteLine("{0}:{1}", host, port);
 		}
-
-
-		private static void SendMailByMyProcess()
-		{
-			// attachment does not work
-//			string mailto =
-//				@"mailto:jeff.stuyvesant@cyberstudio.pro?subject=subject of message&Body=This is a body of a message"
-//				+ "&Attachment=D:\\Users\\Jeff\\Documents\\Programming\\VisualStudioProjects\\SendEmail\\SendEmail\\TextFile.txt";
-
-// this will not work as, i guess, the path is too long
-			string body; // =
-//				"User Name\t|\tjeffs@architectsorange.com\r\nUser Id\t|\tTVFGJX5H67P7\r\nAll Users Addins Location\t|\tC:\\ProgramData\\Autodesk\\Revit\\Addins\\2018\r\nCurrent User Addins Location\t|\tC:\\Users\\jeffs\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2018\r\nShared Parameters Filename\t|\tD:\\Users\\Jeff\\OneDrive\\Office\\Drawings & Revit\\Revit Projects\\Revit Configuration\\Share Parameters-1.txt\r\nSub Version Number\t|\t2018.0\r\nVersion Build\t|\t20170223_1515(x64)\r\nRecording Journal Filename\t|\tC:\\Users\\jeffs\\AppData\\Local\\Autodesk\\Revit\\Autodesk Revit 2018\\Journals\\journal.0346.txt\r\nVersion Name\t|\tAutodesk Revit 2018\r\nVersion Number\t|\t2018\r\nActive AddIn Name\t|\tApplication HelpDesk\r\nLanguage\t|\tEnglish_USA\r\nProduct\t|\tRevit\r\nDocument Path Name\t|\tD:\\Users\\Jeff\\OneDrive\\Office\\Drawings & Revit\\Revit Projects\\Revit Configuration\\ConfigTest.rvt\r\nDocument Title\t|\tConfigTest.rvt\r\nActive View\t|\tSheet: CS.0-0 - COVER SHEET(COVER SHEET)\r\nDocument Set\t|\tName\r\nDocument Set Item 0\t|\tAutodesk.Revit.DB.Document (Autodesk.Revit.DB.Document)\r\nLibrary Paths\t|\tPath\r\nLibrary Path 0\t|\tC:\\ProgramData\\Autodesk\\RVT 2018\\Libraries\\US Imperial\\Detail Items\r\nLibrary Path 1\t|\tC:\\ProgramData\\Autodesk\\RVT 2018\\Libraries\\US Imperial\\\r\nServer Network Hosts\t|\tHost Name\r\nLoaded Applications 0\t|\tAutodesk.RaaS.RevitAddin.EApplication (Autodesk.RaaS.RevitAddin.EApplication)\r\nLoaded Applications 1\t|\tAutoLoader.AutoLoader (AutoLoader.AutoLoader)\r\nLoaded Applications 2\t|\tConnectedDesktopUI.ServerApp (ConnectedDesktopUI.ServerApp)\r\nLoaded Applications 3\t|\tAutodesk.CoordinationModelUI.CoordinationModelUIApplication (Autodesk.CoordinationModelUI.CoordinationModelUIApplication)\r\nLoaded Applications 4\t|\tEnergyAnalysis.Application (EnergyAnalysis.Application)\r\nLoaded Applications 5\t|\tFabricationPartBrowser.FabPartBrowserApplication (FabricationPartBrowser.FabPartBrowserApplication)\r\nLoaded Applications 6\t|\tFittingAndAccessoryCalculationUIServers.ServerApp (FittingAndAccessoryCalculationUIServers.ServerApp)\r\nLoaded Applications 7\t|\tBIM.IFC.Export.UI.IFCCommandOverrideApplication (BIM.IFC.Export.UI.IFCCommandOverrideApplication)\r\nLoaded Applications 8\t|\tKeynoteUIServer.ServerApp (KeynoteUIServer.ServerApp)\r\nLoaded Applications 9\t|\tMemberForcesUI.ServerApp (MemberForcesUI.ServerApp)\r\nLoaded Applications 10\t|\tBIM.Numbering.UI.NumberingUIApplication (BIM.Numbering.UI.NumberingUIApplication)\r\nLoaded Applications 11\t|\tPointCloudSnappingServer.Application (PointCloudSnappingServer.Application)\r\nLoaded Applications 12\t|\tPressureLossReport.PressureLossReportApplication (PressureLossReport.PressureLossReportApplication)\r\nLoaded Applications 13\t|\tRebarMorphedServer.RebarUpdateApp (RebarMorphedServer.RebarUpdateApp)\r\nLoaded Applications 14\t|\tSectionProperties.ServerApp (SectionProperties.ServerApp)\r\nLoaded Applications 15\t|\tSiteLinker.SiteExportApp (SiteLinker.SiteExportApp)\r\nLoaded Applications 16\t|\tSpaceNaming.Command (SpaceNaming.Command)\r\nLoaded Applications 17\t|\tRvtParameterOrganizer.ExtApp (RvtParameterOrganizer.ExtApp)\r\nLoaded Applications 18\t|\tDluxMeasure.AppRibbon (DluxMeasure.AppRibbon)\r\nLoaded Applications 19\t|\tRevit.PrintHarness.PrintHarnessApplication (Revit.PrintHarness.PrintHarnessApplication)\r\nLoaded Applications 20\t|\tAutodesk.Revit.UI.Collaborate.CollaborateApplication (Autodesk.Revit.UI.Collaborate.CollaborateApplication)\r\nLoaded Applications 21\t|\tAutodesk.Revit.UI.CollaborateBrowser.CollaborateBrowserApplication (Autodesk.Revit.UI.CollaborateBrowser.CollaborateBrowserApplication)\r\nLoaded Applications 22\t|\tSkyscraperClientHost.SkyscraperClientHostApplication (SkyscraperClientHost.SkyscraperClientHostApplication)\r\nLoaded Applications 23\t|\teTransmitForRevit.eTransmitApplication (eTransmitForRevit.eTransmitApplication)\r\nLoaded Applications 24\t|\tBIMStandardsManager.ExternalApp (BIMStandardsManager.ExternalApp)\r\nLoaded Applications 25\t|\tWorkSharingLib.ExternalApplication (WorkSharingLib.ExternalApplication)\r\nLoaded Applications 26\t|\tWorksharingCommand.WorksharingApp (WorksharingCommand.WorksharingApp)\r\nLoaded Applications 27\t|\tAutodesk.Revit.SmartMonkeyPlugin.SmartMonkeyApplication (Autodesk.Revit.SmartMonkeyPlugin.SmartMonkeyApplication)\r\nLoaded Applications 28\t|\tSharedCreate.ApplicationRibbon (SharedCreate.ApplicationRibbon)\r\nLoaded Applications 29\t|\tCTC.RET.SuiteExternalApp.App (CTC.RET.SuiteExternalApp.App)\r\nLoaded Applications 30\t|\tCTC.RET.SuiteExternalApp.App (CTC.RET.SuiteExternalApp.App)\r\nLoaded Applications 31\t|\tSharedCode.ApplicationRibbon (SharedCode.ApplicationRibbon)\r\nLoaded Applications 32\t|\tDynamo.Applications.VersionLoader (Dynamo.Applications.VersionLoader)\r\nLoaded Applications 33\t|\tAutodesk.Revit.Addin.ExportViewSelector.ExportViewSelectorApp (Autodesk.Revit.Addin.ExportViewSelector.ExportViewSelectorApp)\r\nLoaded Applications 34\t|\tFormItConverter.Ribbon (FormItConverter.Ribbon)\r\nLoaded Applications 35\t|\tHelpDesk.RibbonApp (HelpDesk.RibbonApp)\r\n";
-
-
-// works!!!
-			body =
-				"DIAGNOSTIC INFO\r\n===============\r\nUSER: Cecilia & Cardenas\r\nEMAIL: ccardenas@aoarchitects.com\r\nCOMPUTER: OK-3025\r\nREVIT VERSION: Autodesk Revit 2018 | 20190510_1515(x64)\r\nTOTAL MEMORY: 32 GB\r\nFREE MEMORY: 23.6 GB\r\n\r\nLOCAL MODEL: C:\\KTGY-Revit2018Projects\\170394_Alameda Point_Block 6_ccardenas.rvt\r\nCENTRAL MODEL: \\\\corp.ktgy.com\\global\\Oakland\\Projects\\2017\\170394 Alameda Point TH Block 6\\Construction Drawings\\Revit\\170394_Alameda Point_Block 6.rvt\r\nMODEL FILE SIZE: 322.8 MB\r\nREVIT SERVER ACCELERATOR: OAKREVIT1\r\n";
-
-			string data = Uri.EscapeDataString(body);
-			body = Uri.EscapeUriString(body).Replace("&", "%26");
-
-
-// this works when the body is shorter
-			string mailto =
-				@"mailto:jeff.stuyvesant@cyberstudio.pro?subject=subject of message&Body=";
-
-			mailto = Uri.EscapeUriString(mailto) + body;
-
-			Process myprocess = new Process();
-
-			myprocess.StartInfo.FileName = mailto;
-			myprocess.StartInfo.UseShellExecute = true;
-			myprocess.StartInfo.RedirectStandardOutput = false;
-
-			Console.WriteLine("as data:");
-			Console.WriteLine(data);
-
-
-			Console.WriteLine("\n\nas uri:");
-			Console.WriteLine(mailto);
-
-			myprocess.Start();
-
-		}
-
-
-
-		// other tests
-
 		private static void SendMailBySystemNetMail()
 		{
 			// does not work
