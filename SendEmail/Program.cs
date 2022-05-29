7dbd7d6dcac91442e5742da3e8074f0d22e96be2
Jeff <morbius1st@users.noreply.github.com>
2022-05-29 11:02:01 -0700

    update

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -62,46 +62,6 @@ namespace SendEmail
 		}
 
 
-		private static void SendMailBySystemNetMail()
-		{
-			// does not work
-			string file =
-				"D:\\Users\\Jeff\\Documents\\Programming\\VisualStudioProjects\\SendEmail\\SendEmail\\TextFile.txt";
-			// Create a message and set up the recipients.
-			MailMessage message = new MailMessage(
-				"jeff.stuyvesant@cyberstudio.pro",
-				"jeff.stuyvesant@cyberstudio.pro",
-				"Quarterly data report.",
-				"See the attached spreadsheet.");
-
-			// Create  the file attachment for this e-mail message.
-			Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
-			// Add time stamp information for the file.
-			ContentDisposition disposition = data.ContentDisposition;
-			disposition.CreationDate = System.IO.File.GetCreationTime(file);
-			disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
-			disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
-			// Add the file attachment to this e-mail message.
-			message.Attachments.Add(data);
-
-			//Send the message.
-			SmtpClient client = new SmtpClient("smtp-mail.outlook.com");
-			// Add credentials if the SMTP server requires them.
-			client.Credentials = CredentialCache.DefaultNetworkCredentials;
-
-			try
-			{
-				client.Send(message);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Exception caught in CreateMessageWithAttachment(): {0}",
-					ex.ToString() );
-			}
-
-			data.Dispose();
-		}
-
 
 		private static void SendMailByMyProcess()
 		{
